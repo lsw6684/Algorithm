2f5c5d9dfd48860d9ba2e7042262150cfc65030c
lsw6684 <lsw6684@hanmail.net>
2020-12-29 20:34:53 +0900

    문자열을 정수로 바꾸기

---
+++ Programmers/문자열을 정수로 바꾸기.cpp
@@ -0,0 +1,14 @@
+﻿#include <string>
+#include <iostream>
+using namespace std;
+int solution(string s) {
+    return stoi(s);
+}
+
+int main()
+{
+    string s;
+    cin >> s;
+    cout << solution(s);
+    system("pause");
+}
