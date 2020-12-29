0e1fd5d96cac3c035ef03d711bf6221e0e9ebcab
lsw6684 <lsw6684@hanmail.net>
2020-12-29 19:44:05 +0900

    자릿수 더하기

---
+++ 자릿수 더하기.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+int solution(int n)
+{
+    int answer = 0;
+    while (n > 0)
+    {
+        answer += n % 10;
+        n /= 10;
+    }
+
+    return answer;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    cout<<solution(n);
+    system("pause");
+}
