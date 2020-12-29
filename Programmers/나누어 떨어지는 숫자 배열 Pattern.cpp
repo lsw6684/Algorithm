f5cfd89464a7d52997e1500340b655dd5fc86167
lsw6684 <lsw6684@hanmail.net>
2020-12-29 19:22:10 +0900

    Pattern

---
+++ Programmers/나누어 떨어지는 숫자 배열 Pattern.cpp
@@ -0,0 +1,28 @@
+﻿#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+vector<int> solution(vector<int> arr, int divisor) {
+	vector<int> answer;
+	for (int i=0; i < arr.size(); i++)
+	{
+		if (arr[i] %divisor== 0) answer.push_back(arr[i]);
+	}
+	sort(answer.begin(), answer.end());
+	if (!answer.size()) answer.push_back(-1);
+	
+
+	return answer;
+}
+
+int main()
+{
+	vector<int> arr = { 1, 3, 4, 5, 6, 7, 8, 9 };
+	vector<int> test = solution(arr, 3);
+	for (int i = 0; i < test.size(); i++)
+	{
+		cout << test[i];
+	}
+	system("pause");
+}
