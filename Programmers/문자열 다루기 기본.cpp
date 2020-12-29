5826187e6d82d3eb89789125922b58e7d2401b9c
lsw6684 <lsw6684@hanmail.net>
2020-12-29 20:15:46 +0900

    문자열 다루기 기본

---
+++ Programmers/문자열 다루기 기본.cpp
@@ -0,0 +1,20 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+bool solution(string s) {
+	if (s.length() == 4 || s.length() == 6)
+	{
+		for (int i = 0; i < s.length(); i++)
+			if (!isdigit(s[i]))   return false;
+		return true;
+	}
+	return false;
+}
+
+int main() {
+	string s;
+	cin >> s;
+	cout <<solution(s);
+}
