033118849c54fab0333c496a1431697c7378126d
lsw6684 <lsw6684@hanmail.net>
2020-12-29 19:54:15 +0900

    Pattern

---
+++ Programmers/자릿수 더하기 Pattern.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+int solution(int n)
+{
+	int answer = 0;
+	string s = to_string(n);
+
+	for (int i = 0; i < s.size(); i++)
+		answer += (s[i] - '0');			//아스키코드로 빼준 값을 정수로
+										//기발하다 진짜
+		return answer;
+}
+
+int main() {
+	int test;
+	cin >> test;
+	cout << solution(test);
+	system("pause");
+}
