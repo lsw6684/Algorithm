8c199464a4060fadbf3190eda4550eccf2f8edf5
lsw6684 <lsw6684@hanmail.net>
2020-12-29 20:50:11 +0900

    문자열 내 p와 y의 개수

---
+++ Programmers/문자열 내 p와 y의 개수.cpp
@@ -0,0 +1,25 @@
+﻿#include <string>
+#include <iostream>
+using namespace std;
+
+bool solution(string s)
+{
+    bool answer = true;
+    int a = 0, b = 0;
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] == 'p' || s[i] == 'P') a += 1;
+        if (s[i] == 'y' || s[i] == 'Y') b += 1;
+    }
+    if (a == b) answer = true;
+    else answer = false;
+
+    return answer;
+}
+
+int main() {
+    string s;
+    cin >> s;
+    cout << solution(s);
+    system("pause");
+}
