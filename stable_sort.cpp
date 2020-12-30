ee6d0a5a3fd5482dbb17e4018225a09693f3fefc
lsw6684 <lsw6684@hanmail.net>
2020-12-30 09:58:07 +0900

    Stable_sort with function

---
+++ stable_sort.cpp
@@ -0,0 +1,29 @@
+﻿#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+struct Person {
+	int age;
+	string name;
+	int join;
+};
+bool cmp(const Person& u, const Person& v) {
+	return u.age < v.age;
+}
+int main() {
+	int n;
+	cin >> n;
+
+	vector<Person> a(n);
+	for (int i = 0; i < n; i++) {
+		cin >> a[i].age >> a[i].name;
+	}
+
+	stable_sort(a.begin(), a.end(), cmp);
+
+	for (int i = 0; i < n; i++) {
+		cout << a[i].age << ' ' << a[i].name << '\n';
+	}
+	return 0;
+}
