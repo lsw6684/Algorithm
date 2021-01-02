022bd2224c0a729a5c916586fb45963796beb55a
lsw6684 <lsw6684@hanmail.net>
2021-01-02 12:33:46 +0900

    set, vector, sort, unique and so on

---
+++ Programmers/두 개 뽑아서 더하기 Pattern.cpp
@@ -0,0 +1,33 @@
+﻿#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+vector<int> solution(vector<int> numbers) {
+    vector<int> answer;
+    set<int> s;
+    for (int i = 0; i < numbers.size(); i++)
+    {
+        for (int j = i+1; j < numbers.size(); j++)  //서로 다른 것 선택
+        {
+            s.insert(numbers[i] + numbers[j]);
+        }
+    }
+    answer.assign(s.begin(), s.end());
+
+    return answer;
+}
+
+int main() {
+    vector<int> arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    vector<int> test = solution(arr);
+
+    for (int i = 0; i < test.size(); i++)
+    {
+        cout << test[i] << endl;
+    }
+    system("pause");
+}
---
+++ Programmers/두 개 뽑아서 더하기.cpp
@@ -0,0 +1,31 @@
+﻿#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> numbers) {
+    vector<int> answer;
+    for (int i = 0; i < numbers.size(); i++)
+    {
+        for (int j = i + 1; j < numbers.size(); j++)
+        {
+            answer.push_back(numbers[i] + numbers[j]);
+        }
+    }
+    sort(answer.begin(), answer.end());
+    answer.erase(unique(answer.begin(), answer.end()), answer.end());
+    return answer;
+}
+
+int main() {
+    vector<int> arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    vector<int> test = solution(arr);
+
+    for (int i = 0; i < test.size(); i++)
+    {
+        cout << test[i] << endl;
+    }
+    system("pause");
+}
