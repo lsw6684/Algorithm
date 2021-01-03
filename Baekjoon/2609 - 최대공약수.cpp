b4da15cb1048a641c74fbc05ff9628419e635f39
lsw6684 <lsw6684@hanmail.net>
2021-01-03 16:56:28 +0900

    유클리드 알고리즘

---
+++ Baekjoon/2609 - 최대공약수.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+int gcd(int x, int y)
+{
+	if (y == 0) return x;
+	else return gcd(y, x % y);		//재귀
+}
+int main() {
+	int a, b;
+	cin >> a >> b;
+	cout << gcd(a, b) << endl<<a * b / gcd(a, b);
+	system("pause");
+}
