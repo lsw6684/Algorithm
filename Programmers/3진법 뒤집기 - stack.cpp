4eada84d00ef1051cb2d30d17a9dd0515c1ae59b
lsw6684 <lsw6684@hanmail.net>
2021-01-05 08:14:26 +0900

    yaho~

---
+++ Programmers/3진법 뒤집기 - stack.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <stack>
+#include <math.h>
+using namespace std;
+
+int solution(int n) {
+	int answer = 0;
+	stack<int> s;
+
+	// 3진법 변환
+	while (n > 0)
+	{
+		s.push(n%3);
+		n /= 3;
+	}
+
+	int a = 0;
+	int tmp = 0;
+	while (!s.empty())
+	{
+		tmp=pow(3,a);
+		answer += (tmp * s.top());
+		s.pop();
+		a++;
+	}
+	return answer;
+}
+
+int main() {
+	int n;
+	cin >> n;
+	cout << solution(n);
+	system("pause");
+}
