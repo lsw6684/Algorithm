ea28bf3f529d4f3d0056ab7750cb9bb76a83cfc8
lsw6684 <lsw6684@hanmail.net>
2021-01-04 10:13:49 +0900

    진법 변환

---
+++ Baekjoon/11005 - 진법 변환 2.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main(void)
+{
+	int n, b; cin >> n >> b;
+	stack<char> s;
+	int temp =0;
+	while (n != 0)
+	{
+		if (temp >= 10)
+		{
+			temp += 7;	//9와 'A' 의 아스키코드 차이 7
+		} else temp = n % b;
+		s.push('0' + temp);
+		n /= b;
+	}
+	while (!s.empty())
+	{
+		cout << s.top();
+		s.pop();
+	}
+}
