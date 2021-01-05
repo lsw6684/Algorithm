427fc846789371c5cde9e43b9f95af00471cbb19
lsw6684 <lsw6684@hanmail.net>
2021-01-05 09:37:36 +0900

    string sort

---
+++ Programmers/문자열 내림차순으로 배치하기.cpp
@@ -0,0 +1,18 @@
+﻿#include <string>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+string solution(string s) {
+    sort(s.begin(), s.end());
+    reverse(s.begin(), s.end());
+    return s;
+}
+
+int main() {
+    string s;
+    cin >> s;
+    cout << solution(s);
+    system("pause");
+}
