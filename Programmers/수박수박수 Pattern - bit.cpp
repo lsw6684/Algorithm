96c39ffb93cf8dee74ba5289f22556df3c721ecd
lsw6684 <lsw6684@hanmail.net>
2021-01-05 11:07:29 +0900

    수박수박수박수바악

---
+++ Programmers/수박수박수 Pattern - bit.cpp
@@ -0,0 +1,18 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+string solution(int n) {
+	string answer = "";
+	for (int i = 0; i < n; i++)
+		i & 1 ? answer += "박" : answer += "수";
+	return answer;
+}
+
+int main() {
+	int n;
+	cin >> n;
+	cout << solution(n);
+	system("pause");
+}
---
+++ Programmers/수박수박수.cpp
@@ -0,0 +1,28 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+string solution(int n) {
+    string answer = "";
+    if (n % 2 == 0)
+    {
+        for (int i = 0; i < n / 2; i++)
+            answer += "수박";
+    }
+    else
+    {
+        for (int i = 0; i < n / 2; i++)
+            answer += "수박";
+        answer += "수";
+    }
+
+    return answer;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    cout<<solution(n);
+    system("pause");
+}
