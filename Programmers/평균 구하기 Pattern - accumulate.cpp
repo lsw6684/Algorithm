89ee501075ba1cf269ad6987a9e39eeed6c3b9a7
lsw6684 <lsw6684@hanmail.net>
2021-01-05 10:12:50 +0900

    평균 구하기

---
+++ Programmers/평균 구하기 Pattern - accumulate.cpp
@@ -0,0 +1,16 @@
+﻿#include <vector>
+#include <iostream>
+#include <numeric>
+
+using namespace std;
+
+double solution(vector<int> arr) {
+    return accumulate(arr.begin(), arr.end(), 0)/arr.size();
+}
+//총합 - accumulate(시작, 끝, 총합의 초기 값) 즉, 0이면 순수 합.
+
+int main() {
+    vector<int> arr = { 1, 2, 3, 4 ,5 };
+    cout <<solution(arr);
+    system("pause");
+}
---
+++ Programmers/평균 구하기.cpp
@@ -0,0 +1,19 @@
+﻿#include <vector>
+#include <iostream>
+
+using namespace std;
+
+double solution(vector<int> arr) {
+    double answer = 0;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        answer += arr[i];
+    }
+    return answer / arr.size();
+}
+
+int main() {
+    vector<int> arr = { 1, 2, 3, 4 ,5 };
+    cout <<solution(arr);
+    system("pause");
+}
