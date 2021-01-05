018ce9a5d1e4475cd11be208d45de29fe95f231a
lsw6684 <lsw6684@hanmail.net>
2021-01-05 09:25:45 +0900

    sort with function

---
+++ Programmers/문자열 내 마음대로 정렬하기 - sort.cpp
@@ -0,0 +1,29 @@
+﻿#include <string>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int i;
+bool compare(string a, string b)
+{
+	return a[i] == b[i] ? a < b : a[i] < b[i];
+}
+
+vector<string> solution(vector<string> strings, int n) {
+	i = n;
+	sort(strings.begin(), strings.end(), compare);
+	return strings;
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	vector<string> s = {"sun", "bed", "car"};
+	vector<string> test = solution(s, n);
+	for (int i = 0; i < test.size(); i++)
+	{
+		cout << test[i] << endl;
+	}
+	system("pause");
+}
