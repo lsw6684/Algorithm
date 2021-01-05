668175c32414b97b9ff7064d356fe217520079fc
lsw6684 <lsw6684@hanmail.net>
2021-01-05 09:42:31 +0900

    sort with greater&less

---
+++ Programmers/문자열 내림차순으로 배치하기 Pattern.cpp
@@ -0,0 +1,17 @@
+﻿#include <string>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+string solution(string s) {
+	sort(s.begin(), s.end(), greater<char>());	//less<자료형>() : 오름 차순
+	return s;
+}
+
+int main() {
+	string s;
+	cin >> s;
+	cout << solution(s);
+	system("pause");
+}
