76110d8a3af025541a3328987e188cd5c4c08729
lsw6684 <lsw6684@hanmail.net>
2021-01-07 08:19:57 +0900

    Even and Odd

---
+++ Programmers/짝수와 홀수 Pattern.cpp
@@ -0,0 +1,14 @@
+﻿#include <vector>
+#include <iostream>
+using namespace std;
+
+string solution(int num) {
+    return num % 2 ? "Odd" : "Even";
+}
+
+int main() {
+    int n;
+    cin >> n;
+    cout << solution(n);
+    system("pause");
+}
