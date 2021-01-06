b7cb6cdc1c872f370dabfd90c9b0498b9ee5aa91
lsw6684 <lsw6684@hanmail.net>
2021-01-07 08:25:24 +0900

    직사각형 별찍기

---
+++ Programmers/직사각형 별찍기.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int a, b;
+    cin >> a >> b;
+    for (int i = 0; i < b; i++)
+    {
+        for (int j = 0; j < a; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    system("pause");
+}
