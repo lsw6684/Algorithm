7b5942caeb6e29ea6e18ceac98be2a225a176415
lsw6684 <lsw6684@hanmail.net>
2021-01-07 13:52:37 +0900

    윤년

---
+++ Programmers/2016년.cpp
@@ -0,0 +1,22 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+string solution(int a, int b) {
+    string answer[] = { "FRI","SAT","SUN","MON","TUE","WED","THU" };
+    int sum = 0;
+    int month[] = { 31,29,31,30,31,30,31,31,30,31,30,31 };
+    for (int i = 0; i < a - 1; i++)
+    {
+        sum += month[i];
+    }
+    return answer[(sum + b - 1) % 7];
+}
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    cout << solution(a, b);
+    system("pause");
+}
