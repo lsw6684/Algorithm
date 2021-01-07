88c98ebc9c88aad644ca6c3a27b4b2afb84860fa
lsw6684 <lsw6684@hanmail.net>
2021-01-07 13:17:30 +0900

    완주하지 못한 선수

---
+++ Programmers/완주하지 못한 선수.cpp
@@ -0,0 +1,24 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+string solution(vector<string> p, vector<string> c) {
+	string answer = "";
+	sort(p.begin(), p.end());
+	sort(c.begin(), c.end());
+	for (int i = 0; i < c.size(); i++) {
+		if (p[i] != c[i])
+			return p[i];
+	}
+
+	return p[p.size() - 1];
+}
+
+int main() {
+	vector<string> participant = { "leo", "kiki", "eden" },
+		completion = { "eden","kiki" };
+	cout << solution(participant, completion);
+	system("pause");
+}
