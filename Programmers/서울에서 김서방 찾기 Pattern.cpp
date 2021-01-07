ee34861e5ef4571dcdab120a3b5dc2094a2d983d
lsw6684 <lsw6684@hanmail.net>
2021-01-07 12:29:42 +0900

    find function

---
+++ Programmers/서울에서 김서방 찾기 Pattern.cpp
@@ -0,0 +1,17 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+string solution(vector<string> s) {
+    string answer = "김서방은 ";
+    int pos = find(s.begin(), s.end(), "Kim") - s.begin();
+    answer += to_string(pos) + "에 있다";
+    return answer;
+}
+
+int main() {
+    vector<string> s = { "Jane", "Kim", "Joe" };
+    cout << solution(s);
+    system("pause");
+}
---
+++ Programmers/서울에서 김서방 찾기.cpp
@@ -0,0 +1,19 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+string solution(vector<string> s) {
+    string answer = "김서방은 ";
+
+    for (int i = 0; i < s.size(); i++)
+        if (s[i] == "Kim")    answer += to_string(i);
+    answer += "에 있다";
+    return answer;
+}
+
+int main() {
+    vector<string> s = { "Jane", "Kim", "Joe" };
+    cout << solution(s);
+    system("pause");
+}
