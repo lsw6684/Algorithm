7f33090fef1a30ad50b55a698cbd6297627b62b2
lsw6684 <lsw6684@hanmail.net>
2021-01-08 15:44:29 +0900

    natural number

---
+++ Programmers/자연수 뒤집어 배열로 만들기.cpp
@@ -0,0 +1,23 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+vector<int> solution(long long n) {
+    vector<int> answer;
+    while (n > 0)
+    {
+        answer.push_back(n % 10);
+        n /= 10; 
+    }
+    return answer;
+}
+
+int main() {
+    long long n;
+    cin >> n;
+    vector<int> test = solution(n);
+    for (int i = 0; n > 0; n /= 10, i++)
+        cout << test[i];
+    system("pause");
+}
