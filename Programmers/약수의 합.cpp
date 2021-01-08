01824ae482da9035e3cc92ae8382d32bc5fd06ba
lsw6684 <lsw6684@hanmail.net>
2021-01-08 11:15:17 +0900

    약수

---
+++ Programmers/약수의 합.cpp
@@ -0,0 +1,22 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int solution(int n) {
+    int answer=0;
+    for (int i = 1; i <= n; i++)
+    {
+        if (n % i == 0)
+            answer += i;
+    }
+    return answer;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    cout << solution(n);
+    system("pause");
+}
