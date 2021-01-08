2e65004deee9f1ef9500fad9f138bca76939d78c
lsw6684 <lsw6684@hanmail.net>
2021-01-08 10:49:17 +0900

    시저 암호

---
+++ Programmers/시저 암호.cpp
@@ -0,0 +1,32 @@
+﻿#include <string>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+string solution(string s, int n) {
+	for (int i = 0; i < s.size(); i++)
+	{
+		int tmp = n;
+		if (s[i] == 32) continue;
+		if (s[i] > 64 && s[i] < 91)
+		{
+			if (s[i] + n > 90)
+				tmp -= 26;
+		}
+		else {
+			if (s[i] + n > 122)
+				tmp -= 26;
+		}
+		s[i] += tmp;
+	}
+	return s;
+}
+
+int main() {
+	string s;
+	int n;
+	cin >> s >> n;
+	cout << solution(s, n);
+	system("pause");
+}
