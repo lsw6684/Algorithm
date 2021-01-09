f5a50e3b7886fbb71c8f181d1785c3904f949bb9
lsw6684 <lsw6684@hanmail.net>
2021-01-09 10:51:06 +0900

    stoll & greater

---
+++ Programmers/정수 내림차순으로 배치하기 - stoll.cpp
@@ -0,0 +1,19 @@
+﻿#include <string>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+long long solution(long long n) {
+    long long a = 0;
+    string s = to_string(n);
+    sort(s.begin(), s.end(), greater<long long>());
+    a = stoll(s);
+    return a;
+}
+
+int main() {
+    long long s;
+    cin >> s;
+    cout << solution(s);
+    system("pause");
+}
