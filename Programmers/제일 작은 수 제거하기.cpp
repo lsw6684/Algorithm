7fec2e8a52951f9af5c924d04550299d6b1f7cae
lsw6684 <lsw6684@hanmail.net>
2021-01-09 15:59:27 +0900

    min_element

---
+++ Programmers/제일 작은 수 제거하기.cpp
@@ -0,0 +1,22 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+vector<int> solution(vector<int> arr) {
+    arr.erase(min_element(arr.begin(), arr.end()));
+    if (arr.empty())
+    {
+        arr.push_back(-1);
+        return arr;
+    }else   return arr;
+}
+
+int main() {
+    vector<int> a = {5, 1, 2, 3, 4 };
+    vector<int> t = solution(a);
+    for (int i = 0; i < t.size(); i++)
+        cout << t[i];
+    system("pause");
+}
