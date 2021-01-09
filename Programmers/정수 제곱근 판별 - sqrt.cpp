05997f38d9d00075eb7b5784cd8333318d97084f
lsw6684 <lsw6684@hanmail.net>
2021-01-09 15:22:38 +0900

    pow and sqrt

---
+++ Programmers/정수 제곱근 판별 - sqrt.cpp
@@ -0,0 +1,18 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+long long solution(long long n) {
+    if (sqrt(n) - (int)sqrt(n) == 0) //n이 정수의 제곱인 경우
+        return (sqrt(n) + 1) * (sqrt(n) + 1);
+    return -1;
+}
+
+int main() {
+    long long a;
+    cin >> a;
+    cout << solution(a);
+    system("pause");
+}
---
+++ Programmers/정수 제곱근 판별 Pattern - pow&sqrt.cpp
@@ -0,0 +1,18 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+long long solution(long long n) {
+    long long answer = sqrt(n);
+
+    return powl(answer, 2) == n ? powl(answer + 1, 2) : -1;
+}
+
+int main() {
+    long long a;
+    cin >> a;
+    cout << solution(a);
+    system("pause");
+}
