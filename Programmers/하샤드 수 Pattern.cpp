02a0e08f3df46d795fd7ace0c46b82f10beaf2a0
lsw6684 <lsw6684@hanmail.net>
2021-01-09 14:46:41 +0900

    하샤드 수

---
+++ Programmers/하샤드 수 Pattern.cpp
@@ -0,0 +1,23 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+bool solution(int x) {
+	int tmp = x;
+	int sum = 0;
+	while (tmp > 0)
+	{
+		sum += tmp % 10;
+		tmp /= 10;
+	}
+	return x % sum == 0 ? true : false;
+}
+
+int main()
+{
+	int x;
+	cin >> x;
+	cout << solution(x);
+	system("pause");
+}
