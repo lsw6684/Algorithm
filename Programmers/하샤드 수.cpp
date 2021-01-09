aed0a808bfb7607b1deaa02130ef4869632a98f4
lsw6684 <lsw6684@hanmail.net>
2021-01-09 11:44:03 +0900

    하샤드

---
+++ Programmers/하샤드 수.cpp
@@ -0,0 +1,31 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+bool solution(int x) {
+	bool answer = true;
+	vector <int> tmp;
+	int split = x;
+	int sum = 0;
+
+	while (split > 0)
+	{
+		tmp.push_back(split % 10);
+		split /= 10;
+	}
+	for (int i = 0; i < tmp.size(); i++)
+	{
+		sum += tmp[i];
+	}
+	if (x % sum != 0)	answer=false;
+	return answer;
+}
+
+int main()
+{
+	int x;
+	cin >> x;
+	cout << solution(x);
+	system("pause");
+}
