e178f8406b4bdea282abdfefdd6a2b843152de8b
lsw6684 <lsw6684@hanmail.net>
2021-01-09 20:07:49 +0900

    x만큼 간격이 있는 n개의 숫자

---
+++ Programmers/x만큼 간격이 있는 n개의 숫자.cpp
@@ -0,0 +1,25 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+vector<long long> solution(int x, int n) {
+    vector<long long> answer;
+    int sum = x;
+    for (int i = 0; i < n; i++)
+    {
+        answer.push_back(sum);
+        sum += x;
+
+    }
+    return answer;
+}
+
+int main() {
+    int x, n;
+    cin >> x >> n;
+    vector<long long> test = solution(x, n);
+    for (int i = 0; i < test.size(); i++)
+        cout <<test[i];
+    system("pause");
+}
