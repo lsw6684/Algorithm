76dbe9ce374086bdb6753075acab132491e788a2
lsw6684 <lsw6684@hanmail.net>
2021-01-09 16:52:01 +0900

    유클리드 호제법

---
+++ Programmers/최대공약수와 최소공배수 - 유클리드 호제법.cpp
@@ -0,0 +1,32 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+//유클리드 호제법
+int gcd(int x, int y)
+{
+    if (y == 0) return x;
+    else return gcd(y, x % y);
+}
+
+vector<int> solution(int n, int m) {
+    vector<int> answer;
+
+    answer.push_back(gcd(n, m));
+    answer.push_back(n * m / gcd(n, m));
+
+    return answer;
+}
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    vector<int> test = solution(n, m);
+
+    for (int i = 0; i < test.size(); i++)
+    {
+        cout << test[i];
+    }
+}
