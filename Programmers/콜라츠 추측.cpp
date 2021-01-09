547d878cd8a5d651cc3f1112b28e8aa270ed6489
lsw6684 <lsw6684@hanmail.net>
2021-01-09 19:32:57 +0900

    make 1

---
+++ Programmers/콜라츠 추측.cpp
@@ -0,0 +1,32 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+int solution(int num) {
+	long long a = num;
+	int answer = 0;
+	while (a != 1) {
+		if (a % 2 == 0)
+		{
+			a /= 2;
+		}
+		else
+		{
+			a *= 3;
+			a++;
+		}
+		answer++;
+		if (answer == 500)
+		{
+			return -1;
+		}
+	}
+	return answer;
+}
+
+int main() {
+	int num;
+	cin >> num;
+	cout << solution(num);
+	system("pause");
+}
