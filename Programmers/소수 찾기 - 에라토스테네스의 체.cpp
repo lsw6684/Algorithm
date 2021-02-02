651d33abb2fc5d8aeb4290fddf4405e47116feac
lsw6684 <lsw6684@hanmail.net>
2021-02-03 08:00:54 +0900

    This was really hard for me :(

--- Programmers/소수 찾기 - 에라토스테네스의 체.cpp
+++ Programmers/소수 찾기 - 에라토스테네스의 체.cpp
@@ -3,32 +3,30 @@
 using namespace std;
 
 int solution(int n) {
-    int answer = 0;
-    bool *check= new bool[n+1]; //true 지워짐, false 안 지워짐.
-    check[0] = check[1] = true;
+	int answer = 0;
+	vector<bool> check(n + 1, 0);
 
-    for (int i = 2; i*i < n; i++)
-    {
-        if (check[i] == false) {
-            for (int j = i + i; j <= n; j += i)
-            {
-                check[j] = true;
-            }
-        }
-    }
-    for (int i = 0; i < n; i++)
-        if (check[i] == false)
-            answer++;
+	for (int i = 2; i <= n; i++)
+	{
+		if (check[i])   // true인 경우
+			continue;
+		for (int j = i + i; j <= n; j += i) {
+			check[j] = true;
+		}
+	}
 
-    
+	for (int i = 2; i <= n; i++)
+		if (!check[i])
+			answer++;
 
-    return answer;
+	return answer;
+
 }
 
 int main()
 {
-    int n;
-    cin >> n;
-    cout << solution(n);
+	int n;
+	cin >> n;
+	cout << solution(n);
 
 }
