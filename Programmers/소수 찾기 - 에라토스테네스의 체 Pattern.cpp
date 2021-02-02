80c752a4167a56370074269bb6299d609b5dd350
lsw6684 <lsw6684@hanmail.net>
2021-02-03 08:08:53 +0900

    This was also really hard for me :( but cool, I think

---
+++ Programmers/소수 찾기 - 에라토스테네스의 체 Pattern.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int solution(int n) {
+	int answer = 0;
+	vector<bool> check(n + 1, 1);
+	for (int i = 2; i <= n; i++)
+	{
+		if (check[i])
+		{
+			for (int j = i; j <= n; j += i)
+				check[j] = 0;
+			answer++;
+		}
+	}
+	return answer;
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	cout << solution(n);
+
+}
