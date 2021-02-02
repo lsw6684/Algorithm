613bf1bf15ea3398c47ecda4c7e55125ffb26271
lsw6684 <lsw6684@hanmail.net>
2021-02-03 08:44:53 +0900

    easy

---
+++ Programmers/예산 - sort.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> d, int budget) {
+    int answer = 0;
+    sort(d.begin(), d.end());
+    for (int i = 0; i < d.size(); i++)
+    {
+        if (budget / d[i] > 0)
+        {
+            answer++;
+            budget -= d[i];
+        }
+        else break;
+    }
+    return answer;
+}
+
+int main() {
+    vector<int> d = { 1, 3, 2, 5, 4 };
+    int budget = 9;
+    cout << solution(d, budget);
+    system("pause");
+}
