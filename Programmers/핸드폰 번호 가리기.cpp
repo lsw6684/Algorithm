0c79ea2d5b51367c736e7dea807c554d004d3b53
lsw6684 <lsw6684@hanmail.net>
2021-02-03 09:48:20 +0900

    아 귀찮아

---
+++ Programmers/핸드폰 번호 가리기.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+string solution(string phone_number) {
+	for (int i = 0; i < phone_number.size() - 4; i++)
+		phone_number[i] = '*';
+
+	return phone_number;
+}
+
+int main() {
+	string test("01055536684");
+	cout << solution(test);
+	system("pause");
+}
