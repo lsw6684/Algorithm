b6dc1a6034b708daaf4ba164d3a3f03af39d5bdd
lsw6684 <lsw6684@hanmail.net>
2021-02-08 20:44:26 +0900

    find function

---
+++ Baekjoon/문자열/10809 - 알파벳 찾기 find.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+    string s;
+    string alphabet = "abcdefghijklmnopqrstuvwxyz";
+    cin >> s;
+    for (int i = 0; i < alphabet.length(); i++)
+        cout << (int)s.find(alphabet[i]) << " ";
+    system("pause");
+}
