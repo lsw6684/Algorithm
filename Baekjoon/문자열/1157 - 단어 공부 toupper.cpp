92bbfc9b05aef259959d0bfb06a58ca94aad1ac1
lsw6684 <lsw6684@hanmail.net>
2021-02-09 17:57:58 +0900

    toupper

---
+++ Baekjoon/문자열/1157 - 단어 공부 toupper.cpp
@@ -0,0 +1,31 @@
+﻿#include <algorithm>
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string s;
+    int alphabet[26] = { 0 };
+    int max = 0, cnt = 0;
+    int target = 0;
+
+    cin >> s;
+    transform(s.begin(), s.end(), s.begin(), (int(*)(int))toupper);
+    for (int i = 0; i < s.length(); i++)
+    {
+        alphabet[s[i] - 'A']++;
+    }
+    for (int i = 0; i < 26; i++)
+    {
+        if (max < alphabet[i])
+        {
+            cnt = 0;
+            max = alphabet[i];
+            target = i;
+        }
+        if (max == alphabet[i]) cnt++;
+    }
+    if (cnt > 1)   cout << "?";
+    else           cout << (char)(target + 'A');
+    system("pause");
+}
