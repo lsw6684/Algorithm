6e27e8e6a75d73119c8e7d520a9ba8a7a30b03d7
lsw6684 <lsw6684@hanmail.net>
2021-02-09 11:56:09 +0900

    string repetition

---
+++ Baekjoon/문자열/2675 - 문자열 반복.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+
+    int r, count;
+    string s;
+
+    cin >> r;
+    for (int i = 0; i < r; i++)
+    {
+        cin >> count >> s;
+        for (int j = 0; j < s.length(); j++)
+        {
+            for (int k = 0; k < count; k++)
+                cout << s[j];
+        }
+        cout << endl;
+    }
+    system("pause");
+}
