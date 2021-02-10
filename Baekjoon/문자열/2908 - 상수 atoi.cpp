1fba5e4a7a88f766a2698da45395e8ddc5c96b36
lsw6684 <lsw6684@hanmail.net>
2021-02-10 10:48:39 +0900

    atoi&c_str()

---
+++ Baekjoon/문자열/2908 - 상수 atoi.cpp
@@ -0,0 +1,21 @@
+﻿#include <algorithm>
+#include <iostream>
+#include <string>
+using namespace std;
+int reverse(int num)
+{
+    string s = to_string(num);
+    reverse(s.begin(), s.end());
+    return atoi(s.c_str()); // atoi는 "c스타일 string" 입력받고 int로 변환하여 리턴
+                            // c_str은 "c스타일 string"으로
+}
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    a = reverse(a);
+    b = reverse(b);
+    cout << (a > b ? a : b);
+    system("pause");
+}
