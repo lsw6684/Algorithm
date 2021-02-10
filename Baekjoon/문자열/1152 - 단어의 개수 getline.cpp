d057a8fc2a4a8daed6aa019c1d8affb072df9041
lsw6684 <lsw6684@hanmail.net>
2021-02-10 10:05:16 +0900

    getline

---
+++ Baekjoon/문자열/1152 - 단어의 개수 getline.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+	string s;
+	int cnt = 1;
+	getline(cin, s);
+	if (s.empty())
+	{
+		cout << "0";
+		return 0;
+	}
+	for (int i = 0; i < s.size(); i++)
+	{
+		
+		if (s[i] == ' ')
+			cnt++;
+	}
+
+	if (s[0] == ' ')	cnt--;
+	if (s[s.size() - 1] == ' ')	cnt--;
+
+	cout << cnt;
+	system("pause");
+}
