ba009f7994c032a7c78ce0001723ac40f85ef3fb
lsw6684 <lsw6684@hanmail.net>
2021-02-11 12:32:11 +0900

    크로아티앙

---
+++ Baekjoon/문자열/2941 - 크로아티아 알파벳.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string s;
+    cin >> s;
+    int cnt = 0;
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] == 'c')
+        {
+            if (s[i + 1] == '=' || s[i + 1] == '-')
+            {
+                cnt++;
+                i++;
+            }
+            else    cnt++;
+        }
+        else if (s[i] == 'd')
+        {
+            if (s[i + 1] == 'z' && s[i + 2] == '=')
+            {
+                cnt++;
+                i += 2;
+            }
+            else if (s[i + 1] == '-') {
+                cnt++;
+                i++;
+            }
+            else    cnt++;
+        }
+        else if (s[i] == 'l' && s[i + 1] == 'j')
+        {
+            cnt++;
+            i++;
+        }
+        else if (s[i] == 'n' && s[i + 1] == 'j')
+        {
+            cnt++;
+            i++;
+        }
+        else if (s[i] == 's' && s[i + 1] == '=')
+        {
+            cnt++;
+            i++;
+        }
+        else if (s[i] == 'z' && s[i + 1] == '=')
+        {
+            cnt++;
+            i++;
+        }
+        else cnt++;
+    }
+    cout << cnt;
+    system("pause");
+}
