e05e2513596f427bdeb31a6a00cf783f1a474328
lsw6684 <lsw6684@hanmail.net>
2021-02-12 16:36:51 +0900

    Check! I'm the Korean top class Hiphop 모범 nobless fabulous turbulence gorgeous but dangerous 난 비트를 비트로 제껴버리는 서비미션 챔피온

---
+++ Baekjoon/문자열/1316 - 그룹 단어 체커.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int num;
+    int sum = 0;
+    string s;
+    bool flag;
+    cin >> num;
+    for (int i = 0; i < num; i++)
+    {
+        cin >> s;
+        flag = true;
+        for (int j = 0; j < s.size(); j++)
+        {
+            if (s[j] != s[j + 1])
+                for (int k = j + 2; k < s.size(); k++)
+                {
+                    if (s[j] == s[k])
+                    {
+                        flag = false;
+                        break;
+                    }
+                }
+        }
+        if (flag)    sum++;
+    }
+    cout << sum;
+}
