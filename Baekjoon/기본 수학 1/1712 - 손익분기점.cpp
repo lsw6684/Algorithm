a0344b731c039e75eb334008017785d3dbd5e957
lsw6684 <lsw6684@hanmail.net>
2021-02-13 09:48:04 +0900

    think the logic first

---
+++ Baekjoon/기본 수학 1/1712 - 손익분기점.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int fCost, vCost, price;
+	
+	cin >> fCost >> vCost >> price;
+	if (vCost >= price)	cout << "-1";
+	else	cout << fCost / (price - vCost) + 1;
+	system("pause");
+}
