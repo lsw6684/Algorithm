6c872e114b9174cb13a57d9f7908492a672c72f1
lsw6684 <lsw6684@hanmail.net>
2021-02-14 15:54:35 +0900

    분수 찾기

---
+++ Baekjoon/기본 수학 1/1193 - 분수찾기.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    int i = 1, sum = 0, r = 0;
+    cin >> n;
+
+    for (;sum+i < n; i++)
+    {
+        sum += i;
+        r = n - sum;
+    }
+    if (n == 1)
+    {
+        cout << "1/1";
+        return 0;
+    }
+    if (i % 2 == 1)
+        cout << i-r+1 << "/" << r;
+    else
+        cout << r << "/" << i - r + 1;
+    system("pause");
+}
