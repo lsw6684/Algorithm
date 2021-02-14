7c183872b8e5975b74b816ce4f895faff0ddd3fb
lsw6684 <lsw6684@hanmail.net>
2021-02-14 16:20:48 +0900

    팽이

---
+++ Baekjoon/기본 수학 1/2869 - 달팽이는 올라가고 싶다.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int V, A, B, sum = 0, cnt = 0;
+    cin >> A >> B >> V;
+
+    if ((V - A) % (A - B) == 0)
+        cout << (V - B) / (A - B);
+    else
+        cout << (V - B) / (A - B) + 1;
+
+    system("pause");
+}
