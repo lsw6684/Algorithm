c81d930c0b08eb51f3603bca2857a1b83472ce9e
lsw6684 <lsw6684@hanmail.net>
2021-02-14 10:51:50 +0900

    등비수여얼

---
+++ Baekjoon/기본 수학 1/2292 - 벌집 등비수열.cpp
@@ -0,0 +1,22 @@
+﻿/*
+1    7    19    37
+   6    12    18
+   1    2     3
+*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n, i = 1;
+    cin >> n;
+
+    for (int sum = 1; sum < n; i++)
+    {
+        sum += i * 6;
+    }
+    if (n == 1) cout << "1";
+    else    cout << i;
+    system("pause");
+
+}
