95ee3079588f488f95a11772375e215a3b4586a8
lsw6684 <lsw6684@hanmail.net>
2021-02-15 12:24:28 +0900

    gogo

---
+++ Baekjoon/기본 수학 1 - 사고력/10250 - ACM 호텔.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int n, H, W, N;
+    int a = 0, b = 0;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> H >> W >> N;
+
+        a = N % H;
+        b = N / H + 1;
+
+        if (a == 0)     a = H;
+        if (!(N % H))   b -= 1;
+        
+        if (b < 10)
+            cout << a << '0' << b << endl;
+        else
+            cout << a << b << endl;
+
+    }
+    system("pause");
+}
