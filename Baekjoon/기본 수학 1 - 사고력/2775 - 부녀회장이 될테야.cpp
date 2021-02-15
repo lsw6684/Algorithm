16ef0d027ff2adfe5640cacf44e854fc96424ca9
lsw6684 <lsw6684@hanmail.net>
2021-02-15 18:23:45 +0900

    하거라

---
+++ Baekjoon/기본 수학 1 - 사고력/2775 - 부녀회장이 될테야.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+int recursive(int a, int b)
+{
+    if (b == 1) return 1;
+    if (a == 0) return b;
+    return (recursive(a - 1, b) + recursive(a, b - 1));
+}
+int main()
+{
+    int r, k, n;
+    cin >> r;
+    for (int i = 0; i < r; i++)
+    {
+        cin >> k >> n;
+        cout << recursive(k, n) << endl;
+    }
+    system("pause");
+}
