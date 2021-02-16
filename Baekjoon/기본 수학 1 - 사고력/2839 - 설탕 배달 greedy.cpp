a92da7899470f5ba99eec561b08a7e98b3bff427
lsw6684 <lsw6684@hanmail.net>
2021-02-16 22:02:26 +0900

    greedy

---
+++ Baekjoon/기본 수학 1 - 사고력/2839 - 설탕 배달 greedy.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int N, five = 0, three = 0;
+    cin >> N;
+    five = N / 5;
+    while (1)
+    {
+        if (five < 0)
+        {
+            cout << "-1";
+            return 0;
+        }
+        if ((N - (5 * five)) % 3 == 0)
+        {
+            three = (N - (5 * five)) / 3;
+            break;
+        }
+        five--;
+    }
+    cout << five + three;
+    system("pause");
+}
