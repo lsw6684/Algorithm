60a0ecfec341d8029fb8a83237edcd7c327e014e
lsw6684 <lsw6684@hanmail.net>
2021-02-18 11:18:08 +0900

    prime number

---
+++ Baekjoon/기본 수학 2 - 소수, 기하/1978 - 소수 찾기.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int r, n;
+    int result = 0;
+    cin >> r;
+    for (int i = 0; i < r; i++)
+    {
+        cin >> n;
+        if (n < 2) continue;
+        if (n == 2)
+        {
+            result++;
+            continue;
+        }
+        for (int j = 2; j < n; j++)
+        {
+            if (n % j == 0)    break;
+            else if (n == j + 1)
+            {
+                result++;
+                break;
+            }
+        }
+    }
+    cout << result;
+    system("pause");
+}
