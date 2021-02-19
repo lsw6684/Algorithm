63b29a4a8ff85415d8699004ebf7ae3713a04f8b
lsw6684 <lsw6684@hanmail.net>
2021-02-19 13:07:28 +0900

    Don't use the endl. this is very slow because of buffer

---
+++ Baekjoon/기본 수학 2 - 소수, 기하/1929 - 소수 구하기.cpp
@@ -0,0 +1,26 @@
+﻿#include<iostream>
+#include<vector>
+#include<cmath>
+
+using namespace std;
+int main()
+{
+    int m, n;
+
+    cin >> m >> n;
+
+    vector<bool> check(n + 1, 1);
+    check[0] = 0;
+    check[1] = 0;
+    for (int i = 2; i <= sqrt(n); i++)
+    {
+        if (check[i])        
+            for (int j = i * 2; j <= n; j += i)
+                check[j] = 0;            
+        
+    }
+    for (int i = m; i <= n; i++)
+        if(check[i])    cout << i << endl;
+}
+
+
