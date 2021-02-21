03831e0b15b9538f13d53125d32e168e573ff1c2
lsw6684 <lsw6684@hanmail.net>
2021-02-21 17:01:54 +0900

    골드바흐흑 골드바흐의 흐흑

---
+++ Baekjoon/기본 수학 2 - 소수, 기하/9020 - 골드바흐의 추측.cpp
@@ -0,0 +1,42 @@
+﻿#include<iostream>
+#include<vector>
+
+int main() {
+    int t, n, flag, max = 0;
+    std::vector<int> wait;
+
+    std::cin >> t;
+
+    for (int i = 0; i < t; i++)
+    {
+        std::cin >> n;
+        if (n > max)    max = n;
+        wait.push_back(n);
+    }
+    std::vector<bool> check(max + 1, 1);
+    for (int i = 2; i < max; i++)
+    {
+        if (check[i])
+            for (int j = i * 2; j <= max; j += i)
+                check[j] = 0;
+    }
+    for (int i = 0; i < wait.size(); i++)
+    {
+        flag = 0;
+        for (int j = wait[i]/2; j >=2; j--)
+        {
+            if (check[j]) {
+                for (int k = j; k < check.size(); k++)
+                {
+                    if (check[k] && wait[i] == j + k) {
+                        std::cout << j << " " << k << "\n";
+                        flag = 1;
+                        break;
+                    }
+                }
+            }
+            if (flag == 1) break;
+        }
+    }
+    system("pause");
+}
