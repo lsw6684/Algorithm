dfe8de4c7be4743e23dee6443b24444ec2f79a65
lsw6684 <lsw6684@hanmail.net>
2021-02-22 11:32:26 +0900

    뭐야이게

---
+++ Baekjoon/기본 수학 2 - 소수, 기하/3009 - 네 번째 점.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <vector>
+int main()
+{
+    int xPoint, yPoint;
+    std::vector<int> x;
+    std::vector<int> y;
+
+    for (int i = 0; i < 3; i++)
+    {
+        std::cin >> xPoint >> yPoint;
+        x.push_back(xPoint);
+        y.push_back(yPoint);
+    }
+
+    if (x[0] == x[1])    xPoint = x[2];
+    else if (x[1] == x[2])    xPoint = x[0];
+    else    xPoint = x[1];
+
+    if (y[0] == y[1])    yPoint = y[2];
+    else if (y[1] == y[2])    yPoint = y[0];
+    else    yPoint = y[1];
+    std::cout << xPoint << ' ' << yPoint;
+
+    system("pause");
+}
