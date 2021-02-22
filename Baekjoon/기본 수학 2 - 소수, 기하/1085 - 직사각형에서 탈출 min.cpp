68d487f7c270fb2728b6e490f62467ca1fa71843
lsw6684 <lsw6684@hanmail.net>
2021-02-22 11:07:23 +0900

    easy

---
+++ Baekjoon/기본 수학 2 - 소수, 기하/1085 - 직사각형에서 탈출 min.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+int main()
+{
+    int x, y, w, h;
+    std::vector<int> min;
+    std::cin >> x >> y >> w >> h;
+    min.push_back(x);
+    min.push_back(y);
+    min.push_back(w - x);
+    min.push_back(h - y);
+    int result = *min_element(min.begin(), min.end());
+    std::cout << result;
+
+    system("pause");
+}
