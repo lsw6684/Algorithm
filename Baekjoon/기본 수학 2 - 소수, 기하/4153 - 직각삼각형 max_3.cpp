bb5a2f17de62ef7b431e0f45dae6a56ff0bd34bd
lsw6684 <lsw6684@hanmail.net>
2021-02-22 12:26:23 +0900

    ㅋㅋㅋㅋㅋ

---
+++ Baekjoon/기본 수학 2 - 소수, 기하/4153 - 직각삼각형 max_3.cpp
@@ -0,0 +1,20 @@
+﻿#include<algorithm>
+#include<iostream>
+#include<vector>
+int main()
+{
+    int a, b, c;
+    std::vector<bool> result;
+    std::vector<int> findMax;
+    while (1)
+    {
+        std::cin >> a >> b >> c;
+        if (!a)    break;
+        else if ((std::max({ a, b, c }) == a && a * a == b * b + c * c) ||
+            (std::max({ a, b, c }) == b && b * b == a * a + c * c) ||
+            (std::max({ a, b, c }) == c && c * c == a * a + b * b))   std::cout << "right\n";
+        else std::cout << "wrong\n";
+
+    }
+    system("pause");
+}
