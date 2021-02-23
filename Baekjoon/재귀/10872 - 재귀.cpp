177e59a62af7ffd154aa0ccdbbdcae09f4deea44
lsw6684 <lsw6684@hanmail.net>
2021-02-23 15:49:16 +0900

    recursive

---
+++ Baekjoon/재귀/10872 - 재귀.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+int factorial(int n)
+{
+    if (n == 1)    return 1;
+    return n * factorial(n - 1);
+}
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    if (n==1||n==0)   printf("1");
+    else {
+        printf("%d", factorial(n));
+    }
+    system("pause");
+}
