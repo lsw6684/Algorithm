1e1370dbb703cbf2ac84e06134622e08f70277ee
lsw6684 <lsw6684@hanmail.net>
2021-02-23 10:39:07 +0900

    기하아악하악

---
+++ Baekjoon/기본 수학 2 - 소수, 기하/3053 - 택시 기하학 PI.cpp
@@ -0,0 +1,18 @@
+﻿#define _CRT_SECURE_NO_WARNINGS	// for VS
+#include <iostream>
+#include <stdio.h>
+#define _USE_MATH_DEFINES	// for Pi. This must come after the "math.h"
+#include <math.h>			// this can be replaced by the <cmath>	
+
+int main() {
+	double r, s1, s2;
+	scanf("%lf", &r);	// lf or le is for double
+	s1 = M_PI * r * r;
+	s2 = r * r * 2;
+
+	printf("%.6f\n", s1);
+	printf("%.6f", s2);
+
+	system("pause");
+}
+
