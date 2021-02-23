a16792f4b61dd351a2cedb5398e61461de6a1323
lsw6684 <lsw6684@hanmail.net>
2021-02-23 16:07:13 +0900

    fibonacci

---
+++ Baekjoon/재귀/10870 - 피보나치 수 5.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+int fibonacci(int n)
+{
+    if (n == 0)    return 0;
+    else if (n == 1)    return 1;
+
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    printf("%d", fibonacci(n));
+    system("pause");
+}
