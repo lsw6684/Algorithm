a30c9bde20e6e0fbac02d2a4abe01ae46f511ed1
lsw6684 <lsw6684@hanmail.net>
2021-02-24 14:50:40 +0900

    난 초딩때 뭐했지

---
+++ Baekjoon/브루트 포스(완전 탐색)/7568 - 덩치.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <vector>
+
+int main()
+{
+    int n, w, h, rate;
+    std::vector<int> ws, hs;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d %d", &w, &h);
+        ws.push_back(w);
+        hs.push_back(h);
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        rate = 1;
+        for (int j = 0; j < n; j++)
+            if (ws[i] < ws[j] && hs[i] < hs[j])
+                rate++;
+        printf("%d ", rate);
+    }
+    system("pause");
+}
