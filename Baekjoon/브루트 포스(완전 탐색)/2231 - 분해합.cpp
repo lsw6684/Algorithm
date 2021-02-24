9c824b500fc3b871780d791f39fcc92113939164
lsw6684 <lsw6684@hanmail.net>
2021-02-24 13:56:37 +0900

    까먹지 말라고 이거

---
+++ Baekjoon/브루트 포스(완전 탐색)/2231 - 분해합.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+int solution(int n)
+{
+    int sum = n;
+    while (n != 0)
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+    return sum;
+
+}
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    for (int i = n / 2; i <= n; i++)
+    {
+        if (solution(i) == n) {
+            printf("%d", i);
+            break;
+        }
+        else if (i == n)
+        {
+            printf("0");
+            break;
+        }
+    }
+    system("pause");
+}
