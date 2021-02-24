84012a616f626381f5063f22f6ac54627096787a
lsw6684 <lsw6684@hanmail.net>
2021-02-25 00:23:40 +0900

    완전 탐색 시야 넓히기

---
+++ Baekjoon/브루트 포스(완전 탐색)/1018 - .체스판 다시 칠하기.cpp
@@ -0,0 +1,71 @@
+﻿#include<algorithm>
+#include<iostream>
+#include<string>
+#include<vector>
+
+const int max = 50;
+std::string board[max];
+std::string w[8] =
+{
+	{"WBWBWBWB"},
+	{"BWBWBWBW"},
+	{"WBWBWBWB"},
+	{"BWBWBWBW"},
+	{"WBWBWBWB"},
+	{"BWBWBWBW"},
+	{"WBWBWBWB"},
+	{"BWBWBWBW"}
+};
+
+std::string b[8] =
+{
+	{"BWBWBWBW"},
+	{"WBWBWBWB"},
+	{"BWBWBWBW"},
+	{"WBWBWBWB"},
+	{"BWBWBWBW"},
+	{"WBWBWBWB"},
+	{"BWBWBWBW"},
+	{"WBWBWBWB"}
+};
+
+int wStart(int i, int j)
+{
+	int result = 0;
+
+	for (int k = i; k < i + 8; k++)
+		for (int l = j; l < j + 8; l++)
+			if (board[k][l] != w[k - i][l - j])
+				result++;
+	return result;
+}
+int bStart(int i, int j)
+{
+	int result = 0;
+
+	for (int k = i; k < i + 8; k++)
+		for (int l = j; l < j + 8; l++)
+			if (board[k][l] != b[k - i][l - j])
+				result++;
+	return result;
+
+}
+
+int main()
+{
+	int n, m;
+	std::vector<int> tmp;
+	std::cin >> n >> m;
+	for (int i = 0; i < n; i++)
+		std::cin >> board[i];
+
+	for (int i = 0; i + 7 < n; i++)
+		for (int j = 0; j + 7 < m; j++)
+		{
+			tmp.push_back(wStart(i, j));
+			tmp.push_back(bStart(i, j));
+		}
+	int min = *min_element(tmp.begin(), tmp.end());
+	std::cout << min;
+	system("pause");
+}
