9c58c66ecea59b6f339aca782e5031fda03116fa
lsw6684 <lsw6684@hanmail.net>
2021-02-24 10:54:25 +0900

    무식

---
+++ Baekjoon/브루트 포스(완전 탐색)/블랙잭 - 2798.cpp
@@ -0,0 +1,23 @@
+﻿#include<iostream>
+#include<vector>
+int main()
+{
+    int n, m, card, max = 0;
+    std::vector<int> cards;
+
+    scanf("%d %d", &n, &m);
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &card);
+        cards.push_back(card);
+    }
+
+    for (int i = 0; i < n-2; i++)
+        for (int j = i+1; j < n; j++)
+            for (int k = j+1; k < n; k++)
+                if (cards[i] + cards[j] + cards[k] > max && cards[i] + cards[j] + cards[k] <= m)
+                    max = cards[i] + cards[j] + cards[k];
+    printf("%d", max);
+    system("pause");
+}
