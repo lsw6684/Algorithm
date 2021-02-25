10a6d7c99a8de80bf5bfb50a554f05c54ca10350
lsw6684 <lsw6684@hanmail.net>
2021-02-25 18:58:16 +0900

    sort

---
+++ Baekjoon/정렬/2750 - 수 정렬하기 sort.cpp
@@ -0,0 +1,18 @@
+﻿#include<iostream>
+#include<vector>
+#include<algorithm>
+int main()
+{
+    int r, n;
+    std::vector<int> goSort;
+    scanf("%d", &r);
+    for (int i = 0; i < r; i++)
+    {
+        scanf("%d", &n);
+        goSort.push_back(n);
+    }
+    sort(goSort.begin(), goSort.end());
+    for (int i = 0; i < goSort.size(); i++)
+        printf("%d\n", goSort[i]);
+    system("pause");
+}
---
+++ Baekjoon/정렬/2751 - 수 정렬하기2 sort.cpp
@@ -0,0 +1,21 @@
+﻿#include<algorithm>
+#include<functional>
+#include<iostream>
+#include<vector>
+
+int main()
+{
+    int r, n;
+    std::vector<int> arr;
+    scanf("%d", &r);
+    for (int i = 0; i < r; i++)
+    {
+        scanf("%d", &n);
+        arr.push_back(n);
+    }
+
+    sort(arr.begin(), arr.end(), std::greater<int>());
+
+    for (int i = 0; i < arr.size(); i++)
+        printf("%d\n", arr[i]);
+}
