bbe08b3933cd4eec0100f58a4575becbf2cf2714
lsw6684 <lsw6684@hanmail.net>
2021-02-25 18:38:50 +0900

    if the 'find' couldn't find the arg, that return 'npos' that -1

---
+++ Baekjoon/브루트 포스(완전 탐색)/1436 - 영화감독 숌 find.cpp
@@ -0,0 +1,25 @@
+﻿#include<iostream>
+#include<string>
+int main()
+{
+    int n, flag = 0;
+    int result = 665;
+
+    scanf("%d", &n);
+
+    while (1)
+    {
+        result++;
+        std::string s = std::to_string(result);
+        if (s.find("666")!=-1)
+        {
+            flag++;
+            if (flag == n)
+            {
+                printf("%d", result);
+                break;
+            }
+        }
+    }
+    system("pause");
+}
