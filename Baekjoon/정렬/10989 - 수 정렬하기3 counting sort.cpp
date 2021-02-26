d11b129c65e1506ad3c7a9afc596f8424c5e815d
lsw6684 <lsw6684@hanmail.net>
2021-02-26 09:37:22 +0900

    counting sort

---
+++ Baekjoon/정렬/10989 - 수 정렬하기3 counting sort.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+int main()
+{
+    int r, n;
+    scanf("%d", &r);
+
+    int arr[10001] = { 0 };
+
+    // 숫자 카운팅
+    for (int i = 0; i < r; i++)
+    {
+        scanf("%d", &n);
+        arr[n]++;
+    }
+
+    // 카운트만큼 출력
+    for (int i = 1; i <= 10000; i++)
+    {
+        while (arr[i] != 0)
+        {
+            printf("%d\n", i);
+            arr[i]--;
+        }
+    }
+    system("puase");
+}
