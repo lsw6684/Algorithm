1e22bbefad87c50587ef9d82d2ce409945512d47
lsw6684 <lsw6684@hanmail.net>
2021-02-26 15:04:01 +0900

    greedy

---
+++ Baekjoon/그리디 알고리즘/11047 - 동전 0.cpp
@@ -0,0 +1,22 @@
+﻿#include<iostream>
+#include<vector>
+int main()
+{
+    int n, k, coin, result = 0, tmp = 0;
+    std::vector<int> vec;
+    scanf("%d %d", &n, &k);
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &coin);
+        vec.push_back(coin);
+    }
+    for (int i = (int)vec.size() - 1; i >= 0; i--)
+        while (k / vec[i] != 0)
+        {
+            tmp = k / vec[i];
+            result += tmp;
+            k %= vec[i];
+        }
+    printf("%d", result);
+    system("pause");
+}
