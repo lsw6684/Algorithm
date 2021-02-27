c28c58609bb753042320fb0cf26859cb7a0b65a6
lsw6684 <lsw6684@hanmail.net>
2021-02-27 10:32:17 +0900

    too many needs

---
+++ Baekjoon/스택/10828 - 스택.cpp
@@ -0,0 +1,38 @@
+﻿#include<iostream>
+#include<stack>
+
+int main()
+{
+    int n, inputInt;
+    std::string input;
+    std::stack<int> s;
+
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        std::cin >> input;
+        if (input == "push")
+        {
+            scanf("%d", &inputInt);
+            s.push(inputInt);
+        }
+        else if (input == "pop")
+            if (s.empty())    printf("-1\n");
+            else
+            {
+                printf("%d\n", s.top());
+                s.pop();
+            }
+        else if (input == "size")
+        {
+            printf("%d\n", s.size());
+        }
+        else if (input == "empty")
+            if (s.empty())    printf("1\n");
+            else             printf("0\n");
+        else if (input == "top")
+            if (s.empty())   printf("-1\n");
+            else    printf("%d\n", s.top());
+    }
+    system("pause");
+}
