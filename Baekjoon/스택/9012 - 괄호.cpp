5fd41b4af7e615171e4b5a31d7c971f3fc56d3c7
lsw6684 <lsw6684@hanmail.net>
2021-02-27 11:25:26 +0900

    annoying

---
+++ Baekjoon/스택/9012 - 괄호.cpp
@@ -0,0 +1,39 @@
+﻿#include<iostream>
+#include<stack>
+
+int main()
+{
+    int n, flag;
+    std::string input;
+
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        flag = 0;
+        std::stack<char> s;
+
+        std::cin >> input;
+        for (int j = 0; j < input.size(); j++)
+        {
+            if (input[j] == '(')    s.push(input[i]);
+            else
+            {
+                if (s.empty())
+                {
+                    printf("NO\n");
+                    flag++;
+                    break;
+                }
+                else
+                {
+                    s.pop();
+                }
+            }
+        }
+        if (flag) continue;
+        if (s.empty())    printf("YES\n");
+        else    printf("NO\n");
+
+    }
+    system("pause");
+}
