2b77a3055be503b0c0eaa39bb84ad86d634e50d5
lsw6684 <lsw6684@hanmail.net>
2021-03-01 19:41:38 +0900

    new challenge

---
+++ Baekjoon/BarkingDog/10808 - 알파벳 개수.cpp
@@ -0,0 +1,21 @@
+﻿#include<iostream>
+#include<vector>
+#define endl "\n"
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(0), cin.tie(0);
+	vector<int> check(26, 0);
+	string s;
+	cin >> s;
+	for (int e : s)
+		check[e - '0' - 49]++;
+
+
+	for (int i = 0; i < check.size(); i++)
+	{
+		cout << check[i] << " ";
+	}
+	system("pause");
+}
