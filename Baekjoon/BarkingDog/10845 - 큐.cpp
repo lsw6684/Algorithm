76e4d31daf05b5d00da1a4b26757613e3fa7020f
lsw6684 <lsw6684@hanmail.net>
2021-03-05 12:29:43 +0900

    Q

---
+++ Baekjoon/BarkingDog/10845 - 큐.cpp
@@ -0,0 +1,49 @@
+﻿#include<iostream>
+#include<queue>
+#define endl "\n"
+
+using namespace std;
+
+int main()
+{
+	queue<int> q;
+	int n, input;
+	string order;
+
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> order;
+
+		if (order == "push")
+		{
+			cin >> input;
+			q.push(input);
+		}
+		else if (order == "pop")
+		{
+			if (q.empty()) cout << "-1" << endl;
+			else {
+				cout << q.front() << endl;
+				q.pop();
+			}
+		}
+		else if (order == "size")	cout << q.size() << endl;
+		else if (order == "empty")
+			if (q.empty())	cout << "1" << endl;
+			else cout << "0" << endl;
+
+		else if (order == "front")
+		{
+			if (q.empty())	cout << "-1" << endl;
+			else			cout << q.front() << endl;
+		}
+		else if (order == "back")
+		{
+			if (q.empty())	cout << "-1" << endl;
+			else			cout << q.back() << endl;
+		}
+	}
+	system("pause");
+}
