15fcc573b0f4445a73348f5c546ef8d19ee8b46a
lsw6684 <lsw6684@hanmail.net>
2021-03-05 17:43:54 +0900

    어렵

---
+++ Baekjoon/BarkingDog/10799 - 쇠 막대기 stack.cpp
@@ -0,0 +1,37 @@
+﻿#include<iostream>
+#include<stack>
+#define endl "\n"
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio, cin.tie(0);
+	string str;
+	stack<int> s;
+	int result = 0;
+
+	cin >> str;
+
+	for (int i = 0; i < str.size(); i++)
+	{
+		if (str[i] == '(')	s.push(i);
+		else
+		{
+			if (s.top() + 1 == i)
+			{
+				s.pop();
+				result += s.size();
+			}
+			else
+			{
+				s.pop();
+				result++;
+			}
+		}
+	}
+
+	
+	cout << result;
+	system("pause");
+}
