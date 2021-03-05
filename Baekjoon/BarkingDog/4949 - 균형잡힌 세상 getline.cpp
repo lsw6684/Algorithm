4108ebb7893608ea11d349f8cc00a5a1add1c661
lsw6684 <lsw6684@hanmail.net>
2021-03-05 17:05:16 +0900

    getline 사용 주의

---
+++ Baekjoon/BarkingDog/4949 - 균형잡힌 세상 getline.cpp
@@ -0,0 +1,46 @@
+﻿#include<iostream>
+#include<stack>
+#include<string>
+#define endl "\n"
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio, cin.tie(0);
+
+
+	while (1)
+	{
+		stack<char> s;
+		string str;
+		bool check = true;
+		getline(cin, str);
+		if (str == ".") break;
+		for (auto i : str)
+		{
+			if (i == '(' || i == '[')  s.push(i);
+			else if (i == ')')
+			{
+				if (s.empty() || s.top() != '(')
+				{
+					check = false;
+					break;
+				}
+				s.pop();
+			}
+			else if (i == ']')
+			{
+				if (s.empty() || s.top() != '[')
+				{
+					check = false;
+					break;
+				}
+				s.pop();
+			}
+		}
+		if (!s.empty())  check = false;
+		if (check) cout << "yes" << endl;
+		else cout << "no" << endl;
+	}
+	system("pause");
+}
