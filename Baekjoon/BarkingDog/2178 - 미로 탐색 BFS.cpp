8512fc4cd72aaaf625beb02bf44038f694086840
lsw6684 <lsw6684@hanmail.net>
2021-03-09 12:33:50 +0900

    soooo difficult

---
+++ Baekjoon/BarkingDog/2178 - 미로 탐색 BFS.cpp
@@ -0,0 +1,39 @@
+﻿#include<iostream>
+#include<queue>
+#define X first
+#define Y second
+#define endl '\n'
+using namespace std;
+
+string board[102];
+int dist[102][102];
+int n, m;
+int dx[4] = { 1, 0, -1, 0 };
+int dy[4] = { 0, 1, 0, -1 };
+	
+int main()
+{
+	ios::sync_with_stdio, cin.tie(0);
+	cin >> n >> m;
+	for (int i = 0; i < n; i++)
+		cin >> board[i];
+	for (int i = 0; i < n; i++) fill(dist[i], dist[i] + m, -1);
+	queue<pair<int, int>> Q;
+	Q.push({ 0,0 });
+	dist[0][0] = 0;
+	while(!Q.empty())
+	{
+		pair<int, int> cur = Q.front(); Q.pop();
+		for (int dir = 0; dir < 4; dir++) {
+			int nx = cur.X + dx[dir];
+			int ny = cur.Y + dy[dir];
+			if(nx < 0 || ny < 0 || nx >= n || ny >= m ||
+				dist[nx][ny] >= 0|| board[nx][ny] != '1')	continue;
+			dist[nx][ny] = dist[cur.X][cur.Y] + 1;
+			Q.push({ nx,ny });
+		}
+
+	}
+	cout << dist[n - 1][m - 1]+1;
+	system("pause");
+}
