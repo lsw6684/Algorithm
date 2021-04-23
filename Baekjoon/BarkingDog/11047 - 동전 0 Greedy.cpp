3b831d3b19c90af488435c3775c3de7f6da4bfa6
lsw6684 <lsw6684@hanmail.net>
2021-04-23 16:35:54 +0900

    simple greedy

---
+++ Baekjoon/BarkingDog/11047 - 동전 0 Greedy.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+#define endl '\n'
+using namespace std;
+
+int main()
+{
+	int n, k, tmp, result=0, r;
+	vector<int> v;
+	cin >> n >> k;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> tmp;
+		v.push_back(tmp);
+	}
+	for (int i = v.size()-1; i >=0; i--)
+	{
+		if (k / v[i] != 0)
+		{
+			result += k / v[i];
+			k = k % v[i];
+		}
+	}
+	cout << result;
+	system("pause");
+}
