c2fa03586a8da17d64ce204cf848c5392ea8b736
SeungWon Lee <lsw6684@hanmail.net>
2021-05-06 15:54:50 +0900

    Delete 두 개 뽑아서 더하기.py

--- Programmers/두 개 뽑아서 더하기.py
+++
@@ -1,8 +0,0 @@
-def solution(numbers):
-    answer = []
-    for x, a in enumerate(numbers):
-        for y, b in enumerate(numbers):
-            if x == y:
-                continue;
-            answer.append(a+b)
-    return sorted(list(set(answer)))
