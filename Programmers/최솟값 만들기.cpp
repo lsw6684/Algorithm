3c47c191013ad8392834257496efda95a7da7455
SeungWon Lee <lsw6684@hanmail.net>
2021-05-22 14:38:01 +0900

    Create 최솟값 만들기.cpp

---
+++ Programmers/최솟값 만들기.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <functional>
+using namespace std;
+
+int solution(vector<int> A, vector<int> B)
+{
+    int answer = 0;
+    sort(A.begin(), A.end());
+    sort(B.begin(), B.end(), greater<int>());
+    for(int i = 0 ; i < A.size(); i++)
+        answer += A[i]*B[i];
+
+
+    return answer;
+}
