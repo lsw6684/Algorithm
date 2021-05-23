ecb88009e834d5662a04ae8791c92f1a637ae22e
SeungWon Lee <lsw6684@hanmail.net>
2021-05-23 15:12:20 +0900

    DP 어렵당..

---
+++ Baekjoon/DP/1463 - 1로 만들기.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int d[1000002];
+
+int main()
+{
+	int n;
+	cin >> n;
+	for (int i = 2; i <= n; i++)
+	{
+		d[i] = d[i - 1] + 1;
+		if (i % 2 == 0)
+			d[i] = min(d[i], d[i / 2] + 1);
+		if (i % 3 == 0)
+			d[i] = min(d[i], d[i / 3] + 1);
+	}
+	cout << d[n];
+
+}
