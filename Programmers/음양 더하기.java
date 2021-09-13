d328737f1bd9411d17f74884ec36ef0f0c4e4f96
lsw6684 <lsw6684@hanmail.net>
2021-09-13 11:35:36 +0900

    삼항연산자 디폴트

---
+++ Programmers/음양 더하기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        for(int i = 0; i < absolutes.length; i++)
+            answer += absolutes[i] * (signs[i]?1:-1);
+        return answer;
+    }
+}
