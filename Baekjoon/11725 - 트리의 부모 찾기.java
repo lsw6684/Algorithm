dc6dae2403e88fa66c5820768f64baf1b3a8d6f0
lsw6684 <lsw6684@hanmail.net>
2021-10-09 13:23:08 +0900

    어려워

---
+++ Baekjoon/11725 - 트리의 부모 찾기.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+
+public class Main {
+	static int n;
+	static ArrayList<Integer>[] list;
+	static int[] parents;
+	static boolean[] check;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int n = Integer.parseInt(br.readLine());
+		
+		list = new ArrayList[n+1];
+		parents = new int[n+1];
+		check = new boolean[n+1];
+		
+		for(int i = 1; i <= n; i++)
+			list[i] = new ArrayList<>();
+		
+		for(int i = 1; i < n; i++)
+		{
+			String[] s = br.readLine().split(" ");
+			int a = Integer.parseInt(s[0]);
+			int b = Integer.parseInt(s[1]);
+			
+			list[a].add(b);
+			list[b].add(a);
+		}
+		
+		for(int i = 1; i <= n; i++)
+			if(!check[i])
+				dfs(i);
+		
+		for(int i = 2; i <= n; i++)
+			sb.append(parents[i] + "\n");
+		
+		System.out.println(sb);
+	}
+	private static void dfs(int v) {
+		if(check[v])
+			return;
+		check[v] = true;
+		for(int vv : list[v]) {
+			if(!check[vv]) {
+				parents[vv] = v;
+				dfs(vv);
+			}
+		}
+	}
+}
