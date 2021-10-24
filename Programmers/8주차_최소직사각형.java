487c9405f4cb42d1e129b4f87c8d4b1743c283db
lsw6684 <lsw6684@hanmail.net>
2021-10-24 22:31:25 +0900

    개선

--- Programmers/8주차_최소직사각형.java
+++ Programmers/8주차_최소직사각형.java
@@ -1,24 +1,14 @@
-import java.util.*;
-
 class Solution {
     public int solution(int[][] sizes) {
-        HashMap<Integer, Integer> hm = new HashMap<>();
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder()), pq2 = new PriorityQueue<>(Collections.reverseOrder());
-        
-        for(int i = 0; i < sizes.length; i++)
-            if(sizes[i][1] > sizes[i][0])
-            {
-                int tmp = sizes[i][0];
-                sizes[i][0] = sizes[i][1];
-                sizes[i][1] = tmp;
-            }
-        for(int i = 0; i < sizes.length; i++)
+        int a = 0, b = 0;
+        for(int[] size : sizes)
         {
-            pq.add(sizes[i][0]);
-            pq2.add(sizes[i][1]);
+            int mx = Math.max(size[0], size[1]);
+            int mn = Math.min(size[0], size[1]);
+            
+            a = Math.max(a, mx);
+            b = Math.max(b, mn);
         }
-        System.out.println(pq.peek());
-        System.out.println(pq2.peek());
-        return pq.peek() * pq2.peek();
+        return a * b;
     }
 }
