98acea141e785bd399b2d6c5fcde25eae3cfdd57
lsw6684 <lsw6684@hanmail.net>
2021-11-27 13:31:05 +0900

    기초부터 다시

---
+++ Baekjoon/2750 - 수 정렬하기.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+public class Main{
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        
+        int[] arr = new int[n];
+        
+        for(int i = 0; i < n; i++)
+            arr[i] = Integer.parseInt(br.readLine());
+        Arrays.sort(arr);
+        
+        for(int i = 0; i < n; i++)
+            sb.append(arr[i] + "\n");
+        
+        System.out.println(sb);
+        
+    }
+}
