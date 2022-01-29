e25eec20941502d66210ad8ddc1c7497bef85c9b
lsw6684 <lsw6684@hanmail.net>
2022-01-29 13:24:52 +0900

    왜 안 되지.. 다시

--- Programmers/신고 결과 받기.java
+++ Programmers/신고 결과 받기.java
@@ -1,53 +1,31 @@
 import java.util.*;
 
-
-
 class Solution {
-	static int[] answer;
     public int[] solution(String[] id_list, String[] report, int k) {
-        HashMap<String, Integer> hm = new HashMap<>();
-        ArrayList<ArrayList<String>> datas = new ArrayList<ArrayList<String>>();
-        ArrayList<String> data = new ArrayList<>();
-        ArrayList<String> test = new ArrayList<>();
+        int[] answer = new int[id_list.length];
+        
+        Map<String, Integer> index = new HashMap<>();
+        Map<String, List<Integer>> list = new HashMap<>();
         
         for(int i = 0; i < id_list.length; i++)
-        	hm.put(id_list[i], 0);
+        	index.put(id_list[i], i);
         
-    	answer = new int [id_list.length];
-        Arrays.sort(report);
-        
-        for(int i = 0; i < report.length; i++)
-        {
-        	String tmp = report[i];
-        	if(i != report.length - 1 && tmp.equals(report[i+1]))
-        		continue;
-        	String[] names = report[i].split(" ");
-        	String judge = names[0];
+        for(String s : report) {
+        	String a = s.split(" ")[0];
+        	String b = s.split(" ")[1];
         	
-        	data.add(names[1]);
+        	if(!list.containsKey(a))
+        		list.put(a, new ArrayList<>());
         	
-        	if((i != report.length - 1 && !judge.equals(report[i+1].split(" ")[0]))
-        			|| i == report.length - 1) {
-        		datas.add(data);
-        		data.clear();
-        		
-        	}
-        	hm.put(names[1], hm.getOrDefault(names[1], 0) + 1);
+        	if(!list.get(a).contains(index.get(b)))
+        		list.get(a).add(index.get(b));	
         }
         
-        Iterator<String> keys = hm.keySet().iterator();
-
-        while(keys.hasNext()) {
-        	String key = keys.next();
-        	if(hm.get(key) >= k)
-        		test.add(key);
-        }
-        
-        for(int i = 0; i < datas.size(); i++)
-    	{
-        	for(int j = 0; j < test.size(); j++)
-    	}
-        
+        for(int i = 0; i < id_list.length; i++)
+        	if(list.containsKey(id_list[i]) && list.get(id_list[i]).size() >= k)
+        		for(int idx : list.get(id_list[i]))
+        			answer[idx]++;
+        	
         return answer;
     }
 }
