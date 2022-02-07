ffb9d994cacaad5d5f110fc0d1a36f029f04c5d4
lsw6684 <lsw6684@hanmail.net>
2022-02-07 16:48:50 +0900

    refactoring

--- Programmers/실패율.java
+++ Programmers/실패율.java
@@ -1,44 +1,42 @@
 import java.util.*;
-class Solution {
-    public int[] solution(int N, int[] stages) {
-        int[] answer = new int[N];
-        HashMap<Integer, Float> hm = new HashMap<>();
-        int sg = 1;
-        for(int i = 0; i < N; i++) {
-            float num = stages.length;
-        	float tmp = num;
-        	float res = 0;
-            for(int j = 0; j < stages.length; j++) {
-        		if(stages[j] < sg)
-        			num--;
-                if(stages[j] == i+1)
-                    res++;
-            }
-        	tmp = res / num;
-        	hm.put(i+1, tmp);
-        	sg++;
-        }
-        List<HashMap.Entry<Integer, Float>> entry 
-        = new LinkedList<>(hm.entrySet());
-        entry.sort(new Comparator<HashMap.Entry<Integer, Float>>() {
-            @Override
-            public int compare(Map.Entry<Integer, Float> o1, Map.Entry<Integer, Float> o2) {
-                if(o1.getValue() > o2.getValue())
-                	return -1;
-                else if(o1.getValue() < o2.getValue())
-                	return 1;
-                return o1.getKey().compareTo(o2.getKey());
-            	//return (int)(o2.getValue() - o1.getValue());
-            }
-        });
-        int i = 0;
-        for(Map.Entry<Integer, Float> ent : entry)
-        {
 
-        	answer[i] = ent.getKey();
-        	i++;
-        }
-        
-        return answer;
-    }
+class Solution {
+	public int[] solution(int N, int[] stages) {
+		int[] answer = new int[N];
+		HashMap<Integer, Double> hm = new HashMap<>();
+
+		double minus = 0;
+		for (int i = 0; i < N; i++) {
+			double numerator = 0;
+			double denominator = stages.length - minus;
+			for (int j = 0; j < stages.length; j++) {
+				if (stages[j] == i + 1) {
+					numerator++;
+					minus++;
+				}
+			}
+			hm.put(i + 1, numerator / denominator);
+		}
+
+		List<HashMap.Entry<Integer, Double>> entry = new LinkedList<>(hm.entrySet());
+
+		entry.sort(new Comparator<HashMap.Entry<Integer, Double>>() {
+			@Override
+			public int compare(Map.Entry<Integer, Double> o1, Map.Entry<Integer, Double> o2) {
+				if (o1.getValue() > o2.getValue())
+					return -1;
+				else if (o1.getValue() < o2.getValue())
+					return 1;
+				return o1.getKey().compareTo(o2.getKey());
+			}
+		});
+
+		int i = 0;
+		for (Map.Entry<Integer, Double> iter : entry) {
+			answer[i] = iter.getKey();
+			i++;
+		}
+
+		return answer;
+	}
 }
