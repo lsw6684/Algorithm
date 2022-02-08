465aea81fb2eadfd4296a6f47530d8286d85e8b3
lsw6684 <lsw6684@hanmail.net>
2022-02-08 11:19:41 +0900

    엥

--- Programmers/제일 작은 수 제거하기.java
+++ Programmers/제일 작은 수 제거하기.java
@@ -1,20 +1,24 @@
 class Solution {
     public int[] solution(int[] arr) {
-        int mn = 0;
-        int[] answer= {};
-        if(arr.length < 2){
-            answer = new int[arr.length];
-            
-            answer[0] = -1;
-            }
-        else{
-            answer = new int[arr.length-1];
-            
-            for(int i = 0 ; i < arr.length; i++)
-                mn = Math.min(mn, arr[i]);
-            for(int i = 0; i < arr.length-1; i++)
-                answer[i] = arr[i];
-        }
-        return answer;
+    	if(arr.length == 1) {
+    		int[] answer = {-1};
+    		return answer;
+    	}
+    	
+    	int[] answer = new int[arr.length -1];
+    	int mn = Integer.MAX_VALUE;
+    	for(int i : arr)
+    		mn = Math.min(mn, i);
+  
+    	int flag = 0;
+    	for(int i = 0; i < arr.length; i++)
+    		if(mn == arr[i])
+    		{
+    			flag++;
+    			continue;
+    		} else
+    			answer[i - flag] = arr[i];
+    		    		
+    	return answer;
     }
 }
