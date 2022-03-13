eeb176739e8a71a42c8dbac64b6946d93c694ba6
lsw6684 <lsw6684@hanmail.net>
2022-03-13 11:51:55 +0900

    매우 저급한 코드, 리팩토링 시급

--- Programmers/소수 찾기 2.java
+++ Programmers/소수 찾기 2.java
@@ -1,5 +1,3 @@
-package tester;
-
 import java.util.*;
 import java.io.*;
 
@@ -14,61 +12,52 @@ class Solution {
 		char[] tmp = numbers.toCharArray();
 		Arrays.sort(tmp);
 		StringBuffer sb = new StringBuffer(String.valueOf(tmp));
+		prime_vis[1] = true;
 		prime(Integer.parseInt(sb.reverse().toString()));
-		
+
 		int[] tmp_arr = new int[numbers.length()];
-		for(int i = 0; i < numbers.length(); i++)
+		for (int i = 0; i < numbers.length(); i++)
 			tmp_arr[i] = numbers.charAt(i) - '0';
-		
-		for(int i = 0; i < tmp_arr.length; i++)
-		{
+
+		for (int i = 0; i < tmp_arr.length; i++) {
 			output = new int[10000000];
 			vis = new boolean[10000000];
 			permutation(tmp_arr, output, vis, 0, numbers.length(), i + 1);
 		}
-		
+
 		int answer = 0;
-		for(int i : set)
-			if(!prime_vis[i])
+		for (int i : set)
+			if (!prime_vis[i] && i != 0 && i != 1)
 				answer++;
-		
+
 		return answer;
 	}
 
 	static void prime(int v) {
-		for(int i = 2; i <= v; i++)
-			if(!prime_vis[i])
-				for(int j = i + i; j <= v; j += i)
+		for (int i = 2; i <= v; i++)
+			if (!prime_vis[i])
+				for (int j = i + i; j <= v; j += i)
 					prime_vis[j] = true;
 	}
+
 	static void permutation(int[] arr, int[] pre_output, boolean[] vis, int depth, int n, int r) {
-    	if(depth == r)
-    		for(int i = 0; i < depth; i++)
-    			if(r == 1)
-    				set.add(pre_output[i]);
-    	else
-    		for(int i = 0; i < n; i++) {
-    			if(!vis[i])
-    			{
-    				vis[i] = true;
-    				pre_output[depth] = arr[i];
-    				permutation(arr, pre_output, vis, depth + 1, n, r);
-    				vis[i] = false;
-    			}
-    		}
-    		
-    }
-}
-
-public class Main {
-	public static void main(String[] args) throws IOException {
-		Solution ss = new Solution();
-		
-		String s = "17";
-
-		System.out.println(ss.solution(s));
+		if (depth == r && depth == 1)
+			for (int i = 0; i < depth; i++)
+				set.add(pre_output[i]);
+		else if (depth == r) {
+			String tmp = "";
+			for (int i = 0; i < depth; i++)
+				tmp += pre_output[i];
+			set.add(Integer.parseInt(tmp));
+		} else
+			for (int i = 0; i < n; i++) {
+				if (!vis[i]) {
+					vis[i] = true;
+					pre_output[depth] = arr[i];
+					permutation(arr, pre_output, vis, depth + 1, n, r);
+					vis[i] = false;
+				}
+			}
 
 	}
 }
-
-
