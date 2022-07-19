6757a919df1f420d8a5d0ae4de2b60f94612ab6d
lsw6684 <lsw6684@hanmail.net>
2022-07-20 08:12:31 +0900

    투 포인터로 전환

--- Inflearn/두 배열 합치기.java
+++ Inflearn/두 배열 합치기.java
@@ -1,26 +1,31 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Iterator;
 import java.util.Scanner;
 
 public class Main {
+
 	public static void main(String[] args) {
-		ArrayList<Integer> list = new ArrayList<>();
 		Scanner sc = new Scanner(System.in);
 		int tmp = sc.nextInt();
-		
-		for(int i = 0; i < tmp; i++)
-			list.add(sc.nextInt());
+		int p1 = 0, p2 = 0;
+		int[] arr1 = new int[tmp];
+				
+		for(int i = 0; i < arr1.length; i++)
+			arr1[i] = sc.nextInt();
 		
 		tmp = sc.nextInt();
+		int[] arr2 = new int[tmp];
 		
-		for(int i = 0; i < tmp; i++)
-			list.add(sc.nextInt());
+		for(int i = 0; i < arr2.length; i++)
+			arr2[i] = sc.nextInt();
 		
-		Collections.sort(list);
-		
-		Iterator<Integer> iter = list.iterator();
-		while(iter.hasNext())
-			System.out.print(iter.next() + " ");
+		while(p1 < arr1.length && p2 < arr2.length)
+			if(arr1[p1] < arr2[p2])
+				System.out.print(arr1[p1++] + " ");
+			else
+				System.out.print(arr2[p2++] + " ");
+		while(p1 < arr1.length)
+			System.out.print(arr1[p1++] + " ");
+		while(p2 < arr2.length)
+			System.out.print(arr2[p2++] + " ");
 	}
+
 }
