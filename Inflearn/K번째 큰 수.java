2a8d9d5140bc10e864c22056b4f1b1716172ea35
lsw6684 <lsw6684@hanmail.net>
2022-07-29 00:21:08 +0900

    백트래킹 성공

--- Inflearn/K번째 큰 수.java
+++ Inflearn/K번째 큰 수.java
@@ -4,33 +4,48 @@ import java.util.TreeSet;
 
 public class Main {
 	static TreeSet<Integer> ts = new TreeSet<>(Collections.reverseOrder());
+	static int[] tmpArr = new int[3];
 	static int[] arr;
 	static boolean[] vis;
-	static int N, K;
-
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
-		K = sc.nextInt();
-		arr = new int[N];
-		vis = new boolean[N];		
-		for(int i = 0; i < N; i++)
-			arr[i] = sc.nextInt();
+	static int n;
+	
+	static void bt(int idx)	{
+		if(idx == 3)
+		{
+			int tmp = 0;
+			for(int i = 0; i < 3; i++)
+				tmp += tmpArr[i];
+			ts.add(tmp);
+			return;
+		}
 		
-		for(int i = 0; i < N - 2; i++)
-			for(int j = i + 1; j < N - 1; j++)
-				for(int l = j + 1; l < N; l++)
-					ts.add(arr[i] + arr[j] + arr[l]);
-		
-		if(ts.size() < K)
-			System.out.println(-1);
-		else			
-			for(int i = 0; i < K; i++)
+		for(int i = idx; i < n; i++)
+			if(!vis[i])
 			{
-				ts.pollFirst();
-				if(i == K - 2)
-					System.out.println(ts.first());
+				vis[i] = true;
+				tmpArr[idx] = arr[i];
+				bt(idx + 1);
+				vis[i] = false;
 			}
 	}
-	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		int k = sc.nextInt();
+		arr = new int[n];
+		vis = new boolean[n];
+		
+		for(int i = 0; i < n; i++)
+			arr[i] = sc.nextInt();
+		
+		bt(0);
+		if(k > ts.size())
+			System.out.println(-1);
+		else
+		{
+			for(int i = 0; i < k - 1; i++)
+				ts.pollFirst();
+			System.out.println(ts.first());
+		}
+	}
 }
