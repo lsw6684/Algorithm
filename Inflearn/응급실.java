78742dfb209c4421902d14004bcbebbac8d4dfe9
lsw6684 <lsw6684@hanmail.net>
2022-08-02 07:54:28 +0900

    고쳤당..:

--- Inflearn/응급실.java
+++ Inflearn/응급실.java
@@ -1,39 +1,47 @@
-import java.util.PriorityQueue;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
-class Person implements Comparable<Person> {
+class Person {
 	int order;
 	int danger;
-	Person(int order, int danger) {
-		this.order = order;
-		this.danger = danger;
-	}
-	
-	@Override
-	public int compareTo(Person o) {
-		if(this.danger > o.danger
-				|| this.danger == o.danger && this.order < o.order)
-			return -1;
-		else
-			return 1;
+	Person(int o, int d) {
+		order = o;
+		danger = d;
 	}
 }
 
-public class Main{
+public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int m = sc.nextInt();
+		Queue<Person> Q = new LinkedList<>();
+		int N = sc.nextInt();
+		int M = sc.nextInt();
 		int answer = 0;
 		
-		PriorityQueue<Person> pq = new PriorityQueue<>();
+		for(int i = 0; i < N; i++)
+			Q.add(new Person(i, sc.nextInt()));
 		
-		for(int i = 0; i < n; i++)
-			pq.add(new Person(i, sc.nextInt()));
-		
-		while(pq.poll().order != m && !pq.isEmpty())
-			answer++;
-		System.out.println(++answer);
-			
+		while(!Q.isEmpty())
+		{
+			Person tmp = Q.poll();
+			for(Person x : Q)
+			{
+				if(x.danger > tmp.danger) {
+					Q.add(tmp);
+					tmp = null;
+					break;
+				}
+			}
+			if(tmp != null)
+			{
+				answer++;
+				if(tmp.order == M) {
+					System.out.println(answer);
+					return;
+				}
+			}
+		}
 	}
 }
+
