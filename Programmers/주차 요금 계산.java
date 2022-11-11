e96dcae8d55f2c1ec73a6fd96d28d1a36f9d4d3f
lsw6684 <lsw6684@hanmail.net>
2022-11-11 23:20:25 +0900

    너무 피곤하당 내일해야지..

---
+++ Programmers/주차 요금 계산.java
@@ -0,0 +1,25 @@
+
+import java.util.HashMap;
+
+class Solution {
+    public int[] solution(int[] fees, String[] records) {
+        int[] answer = {};
+        
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        
+        int bHour = fees[0];
+        int bFee = fees[1];
+        int uHour = fees[2];
+        int uFee = fees[3];
+        
+        return answer;
+    }
+}
+
+public class Main {
+
+	public static void main(String[] args) {
+		Solution s = new Solution();
+
+	}
+}
