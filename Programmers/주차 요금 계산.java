f05cd11a5839c8d9260fcfae9021f6c74a61e4be
lsw6684 <lsw6684@hanmail.net>
2022-11-12 16:42:48 +0900

    그냥 TreeMap 쓸걸

--- Programmers/주차 요금 계산.java
+++ Programmers/주차 요금 계산.java
@@ -1,25 +1,61 @@
-
 import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
 
 class Solution {
-    public int[] solution(int[] fees, String[] records) {
-        int[] answer = {};
-        
-        HashMap<Integer, Integer> hm = new HashMap<>();
-        
-        int bHour = fees[0];
-        int bFee = fees[1];
-        int uHour = fees[2];
-        int uFee = fees[3];
-        
-        return answer;
-    }
-}
+	public Object[] solution(int[] fees, String[] records) {
+		TreeMap<Integer, Integer> sort = new TreeMap<>();
+		HashMap<Integer, Integer> hm = new HashMap<>();
 
-public class Main {
+		int bTime = fees[0];
+		int bFee = fees[1];
+		int uTime = fees[2];
+		int uFee = fees[3];
 
-	public static void main(String[] args) {
-		Solution s = new Solution();
+		// 1. 시간 계산
+		for (int i = 0; i < records.length; i++) {
+			String[] w = records[i].split(" ");
+			String[] time = w[0].split(":");
+			int hour = Integer.parseInt(time[0]);
+			int min = Integer.parseInt(time[1]);
+			int idx = Integer.parseInt(w[1]);
+			String order = w[2];
 
+			int useTime = hour * 60 + min;
+
+			if (order.equals("IN"))
+				hm.put(idx, useTime);
+			else {
+				int cTime = useTime - hm.get(idx);
+				sort.put(idx, sort.getOrDefault(idx, 0) + cTime);
+				hm.remove(idx);
+			}
+		}
+
+		// 2. out이 없는 차량 시간 계산
+		for (Map.Entry<Integer, Integer> entry : hm.entrySet()) {
+			int resTime = 1439 - entry.getValue();
+			int resIdx = entry.getKey();
+
+			sort.put(resIdx, sort.getOrDefault(resIdx, 0) + resTime);
+		}
+
+		// 3. 비용 계산
+		for (Map.Entry<Integer, Integer> entry : sort.entrySet()) {
+			int lastIdx = entry.getKey();
+			int fee;
+			if (entry.getValue() <= bTime)
+				fee = bFee;
+			else {
+				fee =  (entry.getValue() - bTime )/ uTime;
+				if((entry.getValue() - bTime) % uTime != 0)
+					fee++;
+				fee *= uFee;
+				fee += bFee;
+			}
+			sort.put(lastIdx, fee);
+		}
+
+		return sort.values().toArray();
 	}
 }
