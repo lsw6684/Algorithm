bf986d466d5238b76cca9603f87048a465e84536
lsw6684 <lsw6684@hanmail.net>
2023-01-09 07:43:34 +0900

    수정

--- Programmers/개인정보 수집 유효기간.java
+++ Programmers/개인정보 수집 유효기간.java
@@ -1,60 +1,37 @@
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 
 class Solution {
-	public ArrayList<Integer> solution(String today, String[] terms, String[] privacies) {
-        ArrayList<Integer> list = new ArrayList<>();
-        HashMap<Character, Integer> hm = new HashMap<>();
-        int Today = Integer.parseInt(today.replace(".", ""));
-        for(String x : terms) {
-        	
-        	hm.put(x.charAt(0), Integer.parseInt(x.split(" ")[1]));
-        }
-        for(int i = 0; i < privacies.length; i++) {
-        	String[] tmp = privacies[i].split("[.]");
-        	int plus = hm.get(tmp[2].split(" ")[1].charAt(0));
-        	int tmpDay = Integer.parseInt(tmp[2].substring(0, 2)) - 1;
-        	
-        	if(tmpDay == 0) {
-        		tmp[2] = "28";
-        		int mm = Integer.parseInt(tmp[1]) - 1;
-        		if(mm == 0) {
-        			tmp[0] = Integer.toString(Integer.parseInt(tmp[0]) - 1);
-        			tmp[1] = "12";
-        		} else if(mm < 10)
-        			tmp[1] = "0" + Integer.toString(mm); 
-        		else
-        			tmp[1] = Integer.toString(mm); 
-        	} else if(tmpDay < 10)
-        		tmp[2] = "0" + Integer.toString(tmpDay);
-        	else
-        		tmp[2] = Integer.toString(tmpDay);        	
-        	int month = Integer.parseInt(tmp[1]);
-        	        	
-        	int tmpMonth = month + plus;
-        	
-        	if(tmpMonth > 12) {
-    			tmp[0] = Integer.toString(Integer.parseInt(tmp[0]) + tmpMonth / 12);    
-    			tmpMonth %= 12;
-        	}
-        	if(tmpMonth == 0) {
-        		tmp[0] = Integer.toString(Integer.parseInt(tmp[0]) - 1);
-        		tmpMonth = 12;
-        	}        	
-        	if(tmpMonth < 10)
-        		tmp[1] = "0" + Integer.toString(tmpMonth);
-        	else
-        		tmp[1] = Integer.toString(tmpMonth); 
-     
-        	int year = Integer.parseInt(tmp[0] + tmp[1] + tmp[2]);
-        	
-        	
-        	if(Today > year)
-        		list.add(i + 1);
-        	
-        }
-        Collections.sort(list);
-        return list;
+    public ArrayList<Integer> solution(String today, String[] terms, String[] privacies) {
+    	ArrayList<Integer> list = new ArrayList<>();
+    	HashMap<String, Integer> hm = new HashMap<>();
+    	
+    	int todayDate = getDate(today);
+    	
+    	for(String x : terms)
+    	{
+    		String[] tmp = x.split(" ");
+    		hm.put(tmp[0], Integer.parseInt(tmp[1]) * 28);
+    	}
+    	
+    	for(int i = 0; i < privacies.length; i++)
+    	{
+    		int plus = hm.get(privacies[i].split(" ")[1]);
+    		int date = getDate(privacies[i]) - 1;
+    		if(todayDate > plus + date)
+    			list.add(i + 1);
+    	}
+    	
+    	
+    	
+    	return list;
+    }
+    public int getDate(String x) {
+    	String[] date = x.split("[.]");
+    	int year = Integer.parseInt(date[0]) * 12 * 28;
+    	int month = Integer.parseInt(date[1]) * 28;
+    	int day = Integer.parseInt(date[2].substring(0, 2)) + year + month;
+    	
+    	return day;
     }
 }
