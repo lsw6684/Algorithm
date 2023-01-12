ed604f18f46e1a42a2530d6949817aa01f0da68c
lsw6684 <lsw6684@hanmail.net>
2023-01-12 22:56:49 +0900

    -

---
+++ Programmers/과일 장수.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int k, int m, int[] score) {
+    	int answer = 0;
+        Arrays.sort(score);
+        
+        for(int i = score.length - m; i >= 0; i -= m)
+        	answer += score[i] * m;
+        
+        
+        return answer;
+    }
+}
