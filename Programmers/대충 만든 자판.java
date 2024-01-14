1dbe2a7244e74a34b6834145befac01b73e0d0e7
lsw6684 <lsw6684@hanmail.net>
2024-01-15 00:40:19 +0900

    compute 사용

---
+++ Programmers/대충 만든 자판.java
@@ -0,0 +1,40 @@
+import java.util.*;
+class Solution {
+    public ArrayList<Integer> solution(String[] keymap, String[] targets) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        HashMap<Character, Integer> hm = new HashMap<>();
+
+        // 해시 맵에 넣기
+        for(String s: keymap) {
+            for(int i = 0; i < s.length(); i++) {
+                // 람다식 때문(Java 8 이후는 사용 안 해도 됨)
+                final int currentIndex = i; 
+                
+                char input = s.charAt(i);
+                
+                // 값이 없거나, 해당 key의 value가 더 크면 i+1, 그게 아니면 그대로
+                hm.compute(input, (k, v) -> (v == null || v > currentIndex + 1) ? currentIndex + 1 : v);
+                
+            }
+        }
+        
+        for(String target : targets) {
+            int cnt = 0;
+            for(int i = 0; i < target.length(); i++) {
+                char cmp = target.charAt(i);
+                
+                // 값이 없으면 바로 -1 후 break
+                if(!hm.containsKey(cmp)) {
+                    cnt = -1;
+                    break;
+                }
+                
+                // cnt에 연타 수 축적
+                else
+                    cnt += hm.get(cmp);
+            }
+            answer.add(cnt);
+        }
+        return answer;
+    }
+}
--- Programmers/덧칠하기.java
+++ Programmers/덧칠하기.java
@@ -1,9 +1,14 @@
 class Solution {
     public int solution(int n, int m, int[] section) {
         int answer = 1;
+        
+        // 첫 페인트로 초기화
         int idx = section[0] + m - 1;
         
         for(int i = 1; i < section.length; i++) {
+            
+            // 다음 구간이 이전의 페인트 범위 밖이면,
+            // 해당 구역에서 다시 페인트 칠
             if(section[i] > idx) {
                 answer++;
                 idx = section[i] + m - 1;
